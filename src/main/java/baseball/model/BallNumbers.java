0fab34163f565b72bca9c403789bd4e64caa50b7
bky4564 <bky4564@hunet.co.kr>
2022-09-29 10:41:17 +0900

    feat(BallNumbers): BallNumber 일급 컬랙션 class & test 생성

---
+++ src/main/java/baseball/model/BallNumbers.java
@@ -0,0 +1,42 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BallNumbers {
+
+	private final List<BallNumber> ballNumberList;
+
+	public BallNumbers(List<BallNumber> ballNumberList) {
+		this.ballNumberList = ballNumberList;
+	}
+
+	public static BallNumbers of(List<Integer> numbers) {
+		List<BallNumber> ballNumbers = new ArrayList<>();
+		for (Integer number : numbers) {
+			BallNumber ballNumber = new BallNumber(number);
+			validateDuplicateBallNumber(ballNumbers, ballNumber);
+			ballNumbers.add(ballNumber);
+		}
+		return new BallNumbers(ballNumbers);
+	}
+
+	private static void validateDuplicateBallNumber(List<BallNumber> ballNumbers, BallNumber ballNumber) {
+		if (ballNumbers.contains(ballNumber)) {
+			throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+		}
+	}
+
+	public List<BallNumber> getBallNumberList() {
+		return this.ballNumberList;
+	}
+
+	public int getNumberSize() {
+		return this.ballNumberList.size();
+	}
+
+	public int getNumber(int index) {
+		return this.ballNumberList.get(index).getNumber();
+	}
+
+}
